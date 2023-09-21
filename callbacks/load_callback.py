153b5eb9be04adda18ece38e3192c0724f922eb5
ay94 <ahmed.hamadino1994@gmail.com>
2023-09-21 08:26:06 +0100

    load callback

--- callbacks/load_callback.py
+++ callbacks/load_callback.py
@@ -7,69 +7,8 @@ from . import Datasets
 from . import FileHandler
 from . import DatasetConfig
 
-columns_map = {
-    'global_id': 'Global Id', 'token_id': 'Token Id', 'word_id': 'Word Id',
-    'sen_id': 'Sentence Id', 'token_ids': 'Token Selector', 'label_ids': 'Label Id',
-    'first_tokens_freq': 'First Token Frequency', 'first_tokens_consistency': 'First Token Consistency',
-    'first_tokens_inconsistency': 'First Token Inconsistency', 'words': 'Words', 'wordpieces': 'Word Pieces',
-    'tokens': 'Tokens', 'first_tokens': 'First Token', 'truth': 'Ground Truth', 'pred': 'Prediction',
-    'agreement': 'Class Agreement', 'losses': 'Loss', 'tokenization_rate': 'Tokenization Rate',
-    'token_entropy': 'Token Entropy', 'word_entropy': 'Word Entropy', 'tr_entity': 'Entity Truth',
-    'pr_entity': 'Entity Prediction', 'error_type': 'Error Type', 'prediction_entropy': 'Prediction Entropy',
-    'confidences': 'Confidence', 'variability': 'Variability', 'O': 'O Confidence', 'B-PERS': 'B-PERS Confidence',
-    'I-PERS': 'I-PERS Confidence', 'B-ORG': 'B-ORG Confidence', 'I-ORG': 'I-ORG Confidence',
-    'B-LOC': 'B-LOC Confidence',
-    'I-LOC': 'I-LOC Confidence', 'B-MISC': 'B-MISC Confidence', 'I-MISC': 'I-MISC Confidence', '3_clusters': 'K=3',
-    '4_clusters': 'K=4', '9_clusters': 'K=9', 'truth_token_score': 'Truth Silhouette Score',
-    'pred_token_score': 'Prediction Silhouette Score',
-    'x': 'X Coordinate', 'y': 'Y Coordinate', 'pre_x': 'Pretrained X Coordinate', 'pre_y': 'Pretrained Y Coordinate'
-}
 
-characteristics_columns = [
-    'global_id', 'token_id', 'word_id', 'sen_id', 'token_ids', 'label_ids',
-    'words', 'wordpieces', 'tokens', 'first_tokens', 'truth', 'pred', 'agreement',
-    'tr_entity', 'pr_entity', 'error_type',
-    'first_tokens_freq', 'first_tokens_consistency', 'first_tokens_inconsistency',
-    'tokenization_rate',
-    'token_entropy', 'word_entropy',
-    'losses',   'prediction_entropy', 'confidences', 'variability',
-    'truth_token_score', 'pred_token_score',
-    'O', 'B-PERS', 'I-PERS', 'B-ORG', 'I-ORG', 'B-LOC', 'I-LOC', 'B-MISC', 'I-MISC',
-    '3_clusters', '4_clusters', '9_clusters'
-]
-
-decision_columns = [
-    'global_id', 'token_id', 'word_id', 'sen_id', 'token_ids', 'label_ids',
-    'first_tokens_freq', 'first_tokens_consistency', 'first_tokens_inconsistency',
-    'words', 'wordpieces', 'tokens', 'first_tokens', 'truth', 'pred', 'agreement',
-    'losses', 'x', 'y', 'tokenization_rate', 'token_entropy', 'word_entropy', 'tr_entity',
-    'pr_entity', 'error_type', 'prediction_entropy', 'confidences', 'variability',
-    'O', 'B-PERS', 'I-PERS', 'B-ORG', 'I-ORG', 'B-LOC', 'I-LOC', 'B-MISC', 'I-MISC',
-    '3_clusters', '4_clusters', '9_clusters', 'truth_token_score', 'pred_token_score', 'pre_x', 'pre_y'
-]
-
-performance_columns = [
-    'global_id', 'token_id', 'word_id', 'sen_id', 'token_ids', 'token_ids', 'label_ids',
-    'first_tokens_freq', 'first_tokens_consistency', 'first_tokens_inconsistency',
-    'words', 'wordpieces', 'tokens', 'first_tokens', 'truth', 'pred', 'agreement',
-    'losses', 'x', 'y', 'tokenization_rate', 'token_entropy', 'word_entropy', 'tr_entity',
-    'pr_entity', 'error_type', 'prediction_entropy', 'confidences', 'variability',
-    'O', 'B-PERS', 'I-PERS', 'B-ORG', 'I-ORG', 'B-LOC', 'I-LOC', 'B-MISC', 'I-MISC',
-    '3_clusters', '4_clusters', '9_clusters', 'truth_token_score', 'pred_token_score', 'pre_x', 'pre_y'
-]
-
-instance_columns = [
-    'global_id', 'token_id', 'word_id', 'sen_id', 'token_ids', 'token_ids', 'label_ids',
-    'first_tokens_freq', 'first_tokens_consistency', 'first_tokens_inconsistency',
-    'words', 'wordpieces', 'tokens', 'first_tokens', 'truth', 'pred', 'agreement',
-    'losses', 'x', 'y', 'tokenization_rate', 'token_entropy', 'word_entropy', 'tr_entity',
-    'pr_entity', 'error_type', 'prediction_entropy', 'confidences', 'variability',
-    'O', 'B-PERS', 'I-PERS', 'B-ORG', 'I-ORG', 'B-LOC', 'I-LOC', 'B-MISC', 'I-MISC',
-    '3_clusters', '4_clusters', '9_clusters', 'truth_token_score', 'pred_token_score', 'pre_x', 'pre_y'
-]
-
-
-def register_dataset_callbacks(app):
+def register_load_callbacks(app):
     analysis_folder = f'/Users/ay227/Desktop/Final-Year/Datasets'
     fh = FileHandler(analysis_folder)
     dataset_obj = DatasetConfig()
